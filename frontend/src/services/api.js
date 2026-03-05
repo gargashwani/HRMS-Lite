dcb93ceccfb95079267b05e0265655ae4f59e7f4
Ashwani Garg <ashwanigarg@outlook.in>
2026-03-05 20:21:19 +0530

    backend url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://hrms-lite-60vd.onrender.com"
 });
 
 export default API;
